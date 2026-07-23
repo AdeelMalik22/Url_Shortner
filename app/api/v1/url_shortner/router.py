3fc488e5ba034128e771687dffd7be0f858a537f
Subhan Adeel <ms1640070@gmail.com>
2026-07-23 11:55:31 +0500

    added rate limit

--- app/api/v1/url_shortner/router.py
+++ app/api/v1/url_shortner/router.py
@@ -5,13 +5,14 @@ from app.api.v1.url_shortner.models import URL
 from app.api.v1.url_shortner.schema import URLCreate
 from app.services.url_shortner import create_short_url
 from app.utils.db_connection import Base, engine, get_db
-
+from  rateguard import limit
 router = APIRouter()
 Base.metadata.create_all(bind=engine)
 
 
 
 @router.post("/shorten")
+@limit(max_retries=5,ttl=30)
 def shorten(
     data: URLCreate,
     db: Session = Depends(get_db)
